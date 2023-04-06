d7faf2b6d2c45a351b260298c8ad96de2fd11dd6
Milan Suvajac <milansuvajac021@gmail.com>
2023-04-06 21:39:43 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="flex justify-center items-center bg-gradient-to-br from-sky-900 via-blue-400 to-lime-400 h-[100vh] w-full">
+      Weather APP
+    </main>
+  )
+}
+
+export default App
